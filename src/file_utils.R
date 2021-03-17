250de9a6eef1c52db99b807b0c4ed5e6aeb350b4
Jordan S Read <jread@usgs.gov>
2021-03-17 16:35:09 -0500

    Updates with additional files for release; RMSE etc

--- src/file_utils.R
+++ src/file_utils.R
@@ -2,6 +2,7 @@
 convert_feather_file <- function(fileout, feather_fl){
 
   arrow::read_feather(feather_fl) %>%
+    rename(wtemp_obs = wtemp) %>%
     write_csv(fileout)
 }
 
@@ -10,7 +11,24 @@ sf_centroid_metadata <- function(filein){
            crs = 4326)
 }
 
-build_metadata <- function(fileout, orig_meta_fl, release_grid_sf, weather_centroids){
+convert_preds_file <- function(fileout, filein){
+  read_csv(filein) %>%
+    select(site_id,
+           wtemp_EALSTM = `wtemp_predicted-ealstm`,
+           wtemp_XGB = `wtemp_predicted-xgboost`,
+           wtemp_LM = `wtemp_predicted-linear_model`,
+           wtemp_obs = wtemp_actual) %>%
+    write_csv(file = fileout)
+}
+
+build_metadata <- function(fileout, orig_meta_fl, release_grid_sf, weather_centroids, error_fl, cluster_fl){
+
+
+  error_data <- read_csv(error_fl) %>%
+    select(site_id, num_obs = n_obs, RMSE_EALSTM = rmse_ealstm, RMSE_XGB = rmse_xgboost, RMSE_LM = rmse_lm)
+  cluster_data <- read_csv(cluster_fl) %>%
+    select(site_id, cluster_id = `5fold_fold`) %>%
+    mutate(cluster_id = cluster_id + 1)
 
   orig_metadata <- read_csv(orig_meta_fl)
 
@@ -19,13 +37,20 @@ build_metadata <- function(fileout, orig_meta_fl, release_grid_sf, weather_centr
     assign_group_id(polygons = release_grid_sf, use_col = "site_id") %>%
     inner_join(orig_metadata, by = 'site_id') %>%
     inner_join(weather_centroids, by = c('x','y')) %>%
+    left_join(error_data) %>%
+    left_join(cluster_data) %>%
     # now sort
     arrange(group_id, x, desc(y)) %>%
     mutate(predict_id = row_number()) %>% group_by(group_id, x, desc(y)) %>%
     mutate(weather_id = cur_group_id()) %>% ungroup() %>%
-    select(predict_id, weather_id, site_id, group_id, area_m2,
+    mutate(num_obs = case_when(
+      is.na(num_obs) ~ 0,
+      TRUE ~ num_obs
+    )) %>%
+    select(site_id, predict_id, weather_id, group_id, num_obs, area_m2,
            elevation_m = elevation, lake_lon_deg = lon, lake_lat_deg = lat,
-           weather_lon_deg = lon_cell, weather_lat_deg = lat_cell, observed, x, y, group_bbox)
+           weather_lon_deg = lon_cell, weather_lat_deg = lat_cell, num_obs,
+           x, y, RMSE_EALSTM, RMSE_XGB, RMSE_LM, cluster_id, group_bbox)
 }
 
 subset_write <- function(fileout, tbl, remove_cols){
